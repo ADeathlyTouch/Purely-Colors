ba3599a98800d4518ca505ada3e97f2cde1caa5d
A Deathly Touch <adeathlytouch@gmail.com>
2016-11-27 16:21:19 -0600

    First Official Commit Sorta

---
+++ src/main/java/com/deathly/colors/Items/ItemPurifiedDust.java
@@ -0,0 +1,14 @@
+package com.deathly.colors.Items;
+
+import com.deathly.colors.Ref;
+import net.minecraft.item.Item;
+
+/**
+ * Created by Deathly on 11/20/2016 at 2:36 AM.
+ */
+public class ItemPurifiedDust extends Item {
+    public ItemPurifiedDust(String name) {
+        setUnlocalizedName(name);
+        setCreativeTab(Ref.colorstab);
+    }
+}
